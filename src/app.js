c9e5774d1a1f7426c653ebdf9f002510982fcad0
kevinss09 <ksa230@sfu.ca>
2024-08-29 16:17:52 -0700

    Add fixed after testing

--- src/app.js
+++ src/app.js
@@ -1,7 +1,6 @@
 document.querySelector(".calculate-button").addEventListener("click", () => {
 	// Get the type of it
 	const lotType = document.querySelector("#park").value;
-	console.log(lotType);
 
 	// Get and Calculate Date
 	const entryDate = new Date(
@@ -22,26 +21,59 @@ document.querySelector(".calculate-button").addEventListener("click", () => {
 		return;
 	}
 
-	const totalHours = (exitDate - entryDate) / (1000 * 60 * 60);
+	// Check if exit date is before entry date
+	console.log(entryDate);
+	console.log(exitDate);
+	if (exitDate - entryDate == 0) {
+		document.querySelector(".TotalCost").textContent = "$0";
+		return;
+	}
+
+	let totalMinutes = Math.ceil((exitDate - entryDate) / (1000 * 60));
+	let totalHours = totalMinutes / 60;
+	const totalDays = Math.ceil(totalHours / 24);
 	let totalCost = 0;
 
 	// calculate cost based on type
 	switch (lotType) {
 		case "Lot Parking":
-			totalCost = Math.min(9, totalHours * 2);
-			totalCost = Math.min(totalCost, 54);
+			if (totalHours <= 1) {
+				totalCost = 2; // Minimum charge for up to one hour
+			} else {
+				totalCost = Math.min(9, totalHours * 2);
+				if (totalCost == 9 && totalHours > 5) {
+					totalCost = 54;
+				}
+			}
 			break;
 		case "Hourly Parking":
-			totalCost = 2 + Math.max(0, Math.ceil((totalHours - 1) * 2)) * 1;
-			totalCost = Math.min(totalCost, 24);
+			if (totalMinutes <= 60) {
+				totalCost = 2; // First hour charge
+			} else {
+				let additionalHalfHours = Math.ceil((totalMinutes - 60) / 30);
+				totalCost = 2 + additionalHalfHours * 1; // First hour + additional half hours
+			}
+			totalCost = Math.min(totalCost, 24); // Daily maximum
 			break;
 		case "Garage Parking":
-			totalCost = Math.min(12, totalHours * 2);
-			totalCost = Math.min(totalCost, 72);
+			if (totalHours <= 1) {
+				totalCost = 2; // Minimum charge for up to one hour
+			} else {
+				totalCost = Math.min(12, totalHours * 2);
+				if (totalCost == 12 && totalHours > 6) {
+					totalCost = 72;
+				}
+			}
 			break;
 		case "Surface Parking":
-			totalCost = Math.min(10, totalHours * 2);
-			totalCost = Math.min(totalCost, 60);
+			if (totalHours <= 1) {
+				totalCost = 2; // Minimum charge for up to one hour
+			} else {
+				totalCost = Math.min(10, totalHours * 2);
+				if (totalCost == 10 && totalHours > 5) {
+					totalCost = 60;
+				}
+			}
 			break;
 		case "Valet Parking":
 			if (totalHours <= 5) {
